da78567128f6f27754408283020d57f3c3917c36
masataka moroe <masaozozoz91@gmail.com>
2021-03-26 13:37:07 +0900

    Link修正

--- src/components/atoms/index.ts
+++ src/components/atoms/index.ts
@@ -1,3 +1,5 @@
 export { default as PrimaryTitle } from "./PrimaryTitle";
 export { default as PrimaryButton } from "./PrimaryButton";
 export { default as BackLink } from "./BackLink";
+export { default as HomeLink } from "./HomeLink";
+export { default as ArticleLink } from "./ArticleLink";
