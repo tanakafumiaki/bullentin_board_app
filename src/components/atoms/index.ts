3b1313119dd0837ef2f86d997b3ea3ac7bb9d6a0
tanakafumiaki <fumiakishodai@gmail.com>
2021-03-27 18:20:31 +0900

    create_CommentForm

--- src/components/atoms/index.ts
+++ src/components/atoms/index.ts
@@ -2,3 +2,4 @@ export { default as PrimaryTitle } from "./PrimaryTitle";
 export { default as PrimaryButton } from "./PrimaryButton";
 export { default as PrimaryLink } from "./PrimaryLink";
 export { default as EntryForm } from "./EntryForm";
+export { default as CommentForm } from "./CommentForm";
