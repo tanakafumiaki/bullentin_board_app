75bfe78324eb161add8c58f3c05e673809672ea5
tanakafumiaki <fumiakishodai@gmail.com>
2021-03-27 13:25:17 +0900

    add_index.ts_to_default_as

--- src/components/atoms/index.ts
+++ src/components/atoms/index.ts
@@ -1,3 +1,4 @@
 export { default as PrimaryTitle } from "./PrimaryTitle";
 export { default as PrimaryButton } from "./PrimaryButton";
-export { default as BackLink } from "./BackLink";
+export { default as PrimaryLink } from "./PrimaryLink";
+export { default as EntryForm } from "./EntryForm";
