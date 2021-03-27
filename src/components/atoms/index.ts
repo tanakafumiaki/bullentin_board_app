7b77d785e442c2c3f24e3e2757740c72efac69a5
masataka moroe <masaozozoz91@gmail.com>
2021-03-27 10:13:54 +0900

    PrimaryLink作成

--- src/components/atoms/index.ts
+++ src/components/atoms/index.ts
@@ -1,5 +1,3 @@
 export { default as PrimaryTitle } from "./PrimaryTitle";
 export { default as PrimaryButton } from "./PrimaryButton";
 export { default as BackLink } from "./BackLink";
-export { default as HomeLink } from "./HomeLink";
-export { default as ArticleLink } from "./ArticleLink";
