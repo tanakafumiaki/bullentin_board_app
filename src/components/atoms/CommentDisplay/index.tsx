955588d4db7fd836ab808b6585d0e247d2ae7926
tanakafumiaki <fumiakishodai@gmail.com>
2021-03-28 13:38:56 +0900

    create_ArticlePage

--- src/components/atoms/CommentDisplay/index.tsx
+++ src/components/atoms/CommentDisplay/index.tsx
@@ -5,7 +5,9 @@ const CommentDisplay = () => {
     return (
         <div className={styles.textBox}>
             <p className={styles.username}>USERNAME</p>
-            <textarea placeholder={"Comment"} className={styles.textarea}></textarea>
+            <p className={styles.textarea}>「APIからコメント取得」
+                Wow! Just amazing. I love your profile content. Look Forward to see more. Well done!
+            </p>
         </div>
     )
 }
--- src/components/templates/index.ts
+++ src/components/templates/index.ts
@@ -1,1 +1,2 @@
 export { default as PostTemplate } from "./PostTemplate";
+export { default as ArticleTemplate } from "./ArticleTemplate";
