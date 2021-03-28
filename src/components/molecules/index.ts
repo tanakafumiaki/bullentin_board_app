514b112be7b74dd2c74bfc583d48d1161a81e5c7
tanakafumiaki <fumiakishodai@gmail.com>
2021-03-28 15:42:21 +0900

    create_AddButton

--- src/components/molecules/index.ts
+++ src/components/molecules/index.ts
@@ -2,3 +2,4 @@ export { default as Header } from "./Header";
 export { default as ArticleForm } from "./ArticleForm";
 export { default as ArticleDisplay } from "./ArticleDisplay";
 export { default as TopButtons } from "./TopButtons";
+export { default as CommentForm } from "../molecules/CommentForm";
