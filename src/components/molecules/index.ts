9137c381924690ff49440713b442992389215d33
masataka moroe <masaozozoz91@gmail.com>
2021-03-28 12:58:35 +0900

    molecules/index編集2

--- src/components/molecules/index.ts
+++ src/components/molecules/index.ts
@@ -1,4 +1,3 @@
 export { default as Header } from "./Header";
 export { default as ArticleForm } from "./ArticleForm";
-export { default as ArticleDisplay } from "./ArticleDisplay";
-export { default as TopButtons } from "./TopButtons";
+
