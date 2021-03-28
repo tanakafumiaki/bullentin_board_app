188568c250c5eec3851e67dff011e1cf6057ae39
tanakafumiaki <fumiakishodai@gmail.com>
2021-03-28 13:59:47 +0900

    fix_conflict

--- src/components/templates/index.ts
+++ src/components/templates/index.ts
@@ -1,6 +1,3 @@
 export { default as PostTemplate } from "./PostTemplate";
-<<<<<<< HEAD
 export { default as ArticleTemplate } from "./ArticleTemplate";
-=======
 export { default as TopTemplate } from "./TopTemplate";
->>>>>>> develop
