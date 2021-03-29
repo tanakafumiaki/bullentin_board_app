45941a8c593f52c2e01c7f06cc3905b073779967
masataka moroe <masaozozoz91@gmail.com>
2021-03-29 11:27:08 +0900

    レスポンシブ設定＋api編集

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,5 +1,9 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default (req, res) => {
-  res.status(200).json({ name: 'John Doe' })
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default (req: NextApiRequest, res: NextApiResponse) => {
+  res.statusCode = 200
+  res.json({ name: 'John Doe' })
+  const email = req.body.email
 }
