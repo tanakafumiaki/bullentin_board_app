838da2d015bdd3096ac991696c55c2819cd1a53b
tsurutan <tsurutan.android@gmail.com>
2021-04-02 11:42:53 +0900

    feat: 🎸 Add sample async function

--- src/pages/article/index.tsx
+++ src/pages/article/index.tsx
@@ -1,7 +1,24 @@
 import { ArticleTemplate } from "components/templates";
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const ArticlePage: React.VFC = () => {
-    return <ArticleTemplate />;
+  const [articles, setArticles] = useState([]);
+
+  useEffect(() => {
+    (async () => {})();
+  }, []);
+
+  const onClickButton = async (name: string, password: string) => {
+    // 永続化したデータを取得
+    const token = sessionStorage.getItem("token");
+
+    const response = await fetch("/api/v1/lll");
+    const token = response.json();
+
+    // 永続化したデータを取得
+    sessionStorage.setItem("token", token);
+  };
+
+  return <ArticleTemplate articles={articles} />;
 };
 export default ArticlePage;
--- src/pages/signup/index.tsx
+++ src/pages/signup/index.tsx
@@ -1,7 +1,13 @@
 import { SignUpTemplate } from "components/templates";
-import React from "react";
+import React, { useEffect } from "react";
 
 const SignPage: React.VFC = () => {
-    return <SignUpTemplate />;
+  useEffect(() => {
+    console.log("HOGE");
+    (async () => {
+      const response = await fetch("http://localhost:3000");
+    })();
+  }, []);
+  return <SignUpTemplate />;
 };
 export default SignPage;
