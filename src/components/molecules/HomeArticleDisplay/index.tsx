09e754f060f02b42740e8bdd2dd246fdd88d00c6
tanakafumiaki <fumiakishodai@gmail.com>
2021-04-06 12:08:25 +0900

    add_useEffect

--- src/components/molecules/HomeArticleDisplay/index.tsx
+++ src/components/molecules/HomeArticleDisplay/index.tsx
@@ -1,11 +1,34 @@
 import styles from './styles.module.sass';
 import React from "react";
+import { useEffect, useState } from "react";
 
 const HomeArticleDisplay = () => {
+    const [topicsData,changeArticle] = useState([])
+
+    useEffect(()=>{
+        console.log("fetch")
+        // const url = 'https://bullentin-board-api.herokuapp.com/api/v1/topics'
+        // localで確認する場合は以下
+        const url = `http://localhost:3000/api/v1/topics`
+        fetch(url)
+            .then( res => res.json() )
+            .then( res => {
+                changeArticle(res);
+            })
+    },[])
+
     return (
-        <div className={styles.textBox}>
-            <p className={styles.articleWriter}>APIからユーザー名取得</p>
-            <p className={styles.articleTitle}>APIからTitle取得/APIからTitle取得/APIからTitle取得</p>
+        <div>
+            {topicsData.map(
+                topicsData => {
+                    return (
+                        <tr className={styles.textBox}>
+                            <td className={styles.articleWriter}>{topicsData.user_id}</td>
+                            <td className={styles.articleTitle}>{topicsData.title}</td>
+                        </tr>
+                    )
+                }
+            )}
         </div>
     )
 }
