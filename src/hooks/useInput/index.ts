135366fc5ca261df5c2e884cc523e59191444ecc
masataka moroe <masaozozoz91@gmail.com>
2021-04-11 20:29:59 +0900

    loginAPI作成

--- src/hooks/useInput/index.ts
+++ src/hooks/useInput/index.ts
@@ -1,13 +1,10 @@
 import React, { useState } from "react";
-
-const useInput = () => {
+type Response = () => [string, (event: React.ChangeEvent<HTMLInputElement>) => void];
+const useInput: Response = () => {
     const [value, setValue] = useState("")
-
     const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         setValue(event.target.value);
     }
-
     return [value, onChange];
 }
-
 export default useInput;
