e989320f82876d8a85ecb17ff00dc7cf06981d1b
tanakafumiaki <fumiakishodai@gmail.com>
2021-04-15 00:22:24 +0900

    create_PutComment

--- src/components/atoms/index.ts
+++ src/components/atoms/index.ts
@@ -5,3 +5,4 @@ export { default as EntryForm } from "./EntryForm";
 export { default as CommentDisplay } from "./CommentDisplay";
 export { default as ArticleTitleDisplay } from "./ArticleTitleDisplay";
 export { default as AddButton } from "./AddButton";
+export { default as CommentForm } from "./CommentForm";
---
+++ src/components/molecules/AddCommentForm/index.tsx
@@ -0,0 +1,48 @@
+import React, { useEffect, useState }  from "react";
+import { AddButton, CommentForm } from "components/atoms";
+import styles from "./styles.module.sass";
+import { useInput } from "hooks";
+import { useRouter } from "next/router";
+
+const AddCommentForm: React.VFC = () => {
+    const router = useRouter()
+    const [id, setId] = useState<number>()
+    const [text, onChangeComment] = useInput();
+
+    useEffect(() => {
+        // idがqueryで利用可能になったら処理される
+        if (router.asPath !== router.route) {
+            setId(Number(router.query.id));
+        }
+    }, [router]);
+
+    const onClickComment = async () => {
+        const topic_id = {id}.id
+        // console.log(topic_id);
+        // console.log(text);
+        const accessToken = sessionStorage.getItem('access-token');
+        const uid = sessionStorage.getItem('uid');
+        const client = sessionStorage.getItem('client')
+        const response = await fetch("http://localhost:3000/api/v1/comments", {
+            body: JSON.stringify({
+                text: text,
+                topic_id: topic_id
+            }),
+            headers: {
+                'access-token': `${accessToken}`,
+                'uid': `${uid}`,
+                'client': `${client}`,
+                'Content-Type': 'application/json',
+            },
+            method: 'POST'
+        })
+    }
+    return (
+        <div className={styles.textBox}>
+            <p className={styles.user}>USER</p>
+            <AddButton onClick={onClickComment}/>
+            <CommentForm comment={text} onChange={onChangeComment} />
+        </div>
+    );
+};
+export default AddCommentForm;
