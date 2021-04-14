151ad1fcc31c44b732c9a0ed869fb8072d4f0f26
tanakafumiaki <fumiakishodai@gmail.com>
2021-04-14 16:56:43 +0900

    配列の初期値修正

--- src/components/atoms/CommentDisplay/index.tsx
+++ src/components/atoms/CommentDisplay/index.tsx
@@ -4,33 +4,38 @@ import { useRouter } from "next/router";
 
 const CommentDisplay = () => {
     const router = useRouter()
+    const [id, setId] = useState<number>()
     type topicType = any;
     const [topicsData,changeTopics] = useState<topicType>([])
 
+    useEffect(() => {
+        // idがqueryで利用可能になったら処理される
+        if (router.asPath !== router.route) {
+            setId(Number(router.query.id));
+        }
+    }, [router]);
+
     useEffect(()=>{
-        (async () => {
-            const topicId = `${router.query.id}`;
-            // const url = 'https://bullentin-board-api.herokuapp.com/api/v1/topics/${topicId}'
-            // localの場合は以下
-            const url = `http://localhost:3000/api/v1/topics/${topicId}`;
+        if (id) {(async () => {
+            const url = `http://localhost:3000/api/v1/topics/${id}`
+            // const topicsData = await fetch(url);
             //アクセストークンを取得しfecth実行
-            // const accessToken = sessionStorage.getItem('access-token');
-            // const uid = sessionStorage.getItem('uid');
-            // const client = sessionStorage.getItem('client')
-            // const topicsData = await fetch(url,{
-            //     method: 'GET',
-            //     headers: {
-            //         'access-token': `${accessToken}`,
-            //         'uid': `${uid}`,
-            //         'client': `${client}`,
-            //         'Content-Type': 'application/json',
-            //     },
-            // })
-            const topicsData = await fetch(url);
+            const accessToken = sessionStorage.getItem('access-token');
+            const uid = sessionStorage.getItem('uid');
+            const client = sessionStorage.getItem('client')
+            const topicsData = await fetch(url,{
+                method: 'GET',
+                headers: {
+                    'access-token': `${accessToken}`,
+                    'uid': `${uid}`,
+                    'client': `${client}`,
+                    'Content-Type': 'application/json',
+                },
+            })
             const response = await topicsData.json();
             return changeTopics(response);
         })();
-    },[])
+    }},[id])
 
     type commentType = any;
     const [commentsData,changeComment] = useState<commentType>([]);
@@ -39,27 +44,25 @@ const CommentDisplay = () => {
             // const url = 'https://bullentin-board-api.herokuapp.com/api/v1/comments'
             // localで確認する場合は以下
             const url = `http://localhost:3000/api/v1/comments`;
+            // const commentsData = await fetch(url);
             // アクセストークンを取得しfecth実行
-            // const accessToken = sessionStorage.getItem('access-token');
-            // const uid = sessionStorage.getItem('uid');
-            // const client = sessionStorage.getItem('client')
-            // const commentsData = await fetch(url,{
-            //     method: 'GET',
-            //     headers: {
-            //         'access-token': `${accessToken}`,
-            //         'uid': `${uid}`,
-            //         'client': `${client}`,
-            //         'Content-Type': 'application/json',
-            //     },
-            // })
-            const commentsData = await fetch(url);
+            const accessToken = sessionStorage.getItem('access-token');
+            const uid = sessionStorage.getItem('uid');
+            const client = sessionStorage.getItem('client')
+            const commentsData = await fetch(url,{
+                method: 'GET',
+                headers: {
+                    'access-token': `${accessToken}`,
+                    'uid': `${uid}`,
+                    'client': `${client}`,
+                    'Content-Type': 'application/json',
+                },
+            })
             const response = await commentsData.json();
             return changeComment(response);
         })();
     },[])
     const comments = commentsData.filter((item:any) => item.topic_id === topicsData.id)
-
-    // console.table(comments);
 
     return (
         <div>
--- src/components/molecules/ArticleDisplay/index.tsx
+++ src/components/molecules/ArticleDisplay/index.tsx
@@ -4,28 +4,56 @@ import { useRouter } from "next/router";
 
 const ArticleDisplay: React.VFC = () => {
     const router = useRouter();
-    const topicId = router.query.id;
-    console.log(topicId);
+    const [id, setId] = useState<number>()
     type topicType = any;
-    const [topicData,changeTopic] = useState<topicType>([])
+    const [topicData,changeTopic] = useState<topicType>()
+
+    useEffect(() => {
+        // idがqueryで利用可能になったら処理される
+        if (router.asPath !== router.route) {
+            setId(Number(router.query.id));
+        }
+    }, [router]);
 
     useEffect(()=>{
-        (async () => {
-            const url = `http://localhost:3000/api/v1/topics/${topicId}`
-            const topicsData = await fetch(url);
-            const response = await topicsData.json();
-            console.table(topicData)
-            return changeTopic(response);
+        if (id) {(async () => {
+            const url = `http://localhost:3000/api/v1/topics/${id}`
+            // const topicsData = await fetch(url);
+            //アクセストークンを取得しfecth実行
+            const accessToken = sessionStorage.getItem('access-token');
+            const uid = sessionStorage.getItem('uid');
+            const client = sessionStorage.getItem('client')
+            const topicsData = await fetch(url,{
+                method: 'GET',
+                headers: {
+                    'access-token': `${accessToken}`,
+                    'uid': `${uid}`,
+                    'client': `${client}`,
+                    'Content-Type': 'application/json',
+                },
+            })
+            const topicData = await topicsData.json();
+            console.log(topicData)
+            return changeTopic(topicData);
         })();
-    },[])
+    }},[id])
 
-    return (
-        <div className={styles.textBox}>
-            <h1 className={styles.articleTitle}>{topicData.title}</h1>
-            {/*<p className={styles.articleWriter}>{topicData.user.name}</p>*/}
-            <p className={styles.articleDetail}>{topicData.text}</p>
-            <p>test</p>
-        </div>
-    )
+    if(topicData) {
+        return (
+            <div className={styles.textBox}>
+                <h1 className={styles.articleTitle}>{topicData.title}</h1>
+                <p className={styles.articleWriter}>{topicData.user.name}</p>
+                <p className={styles.articleDetail}>{topicData.text}</p>
+            </div>
+        )
+    } else {
+        return (
+            <div className={styles.textBox}>
+                <h1 className={styles.articleTitle}>Roading...</h1>
+                <p className={styles.articleWriter}>Roading...</p>
+                <p className={styles.articleDetail}>Roading...</p>
+            </div>
+        )
+    }
 }
 export default ArticleDisplay;
