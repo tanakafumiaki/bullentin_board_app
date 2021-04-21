034dcb0dd2d68c318a6eaa98f1059db33fa369f8
tanakafumiaki <fumiakishodai@gmail.com>
2021-04-21 22:37:46 +0900

    バリデーション

--- src/components/molecules/LoginEntryForm/index.tsx
+++ src/components/molecules/LoginEntryForm/index.tsx
@@ -15,31 +15,37 @@ const LoginEntryForms: React.VFC = () => {
         //const uid = sessionStorage.getItem('uid');
         //const client = sessionStorage.getItem('client');
         // localで確認する場合は以下
-        // const response = await fetch("http://localhost:3000/api/v1/auth/sign_in", {
-        const response = await fetch("https://bullentin-board-api.herokuapp.com/api/v1/auth/sign_in", {
-            body: JSON.stringify({
-                email,
-                password
-            }),
-            headers: {
-                "Content-Type": "application/json",
-            },
-
-            method: "POST"
-        })
-        if (response.status === 200) {
-            const { data } = await response.json();
-            console.log(data);
-            const accessToken: any = response.headers.get("access-token");
-            const client: any = response.headers.get("client");
-            const uid: any = response.headers.get("uid");
-            sessionStorage.setItem('access-token', accessToken);
-            sessionStorage.setItem('client', client);
-            sessionStorage.setItem('uid', uid);
-            router.push('/home');
+        if(email == "") {
+            alert("emailが入力されていません。")
+        } else if (password == ""){
+            alert("passwordが入力されていません。")
         } else {
-            router.push('/login')
-            alert("エラーが発生しました")
+            // const response = await fetch("http://localhost:3000/api/v1/auth/sign_in", {
+                const response = await fetch("https://bullentin-board-api.herokuapp.com/api/v1/auth/sign_in", {
+                body: JSON.stringify({
+                    email,
+                    password
+                }),
+                headers: {
+                    "Content-Type": "application/json",
+                },
+
+                method: "POST"
+            })
+            if (response.status === 200) {
+                const { data } = await response.json();
+                console.log(data);
+                const accessToken: any = response.headers.get("access-token");
+                const client: any = response.headers.get("client");
+                const uid: any = response.headers.get("uid");
+                sessionStorage.setItem('access-token', accessToken);
+                sessionStorage.setItem('client', client);
+                sessionStorage.setItem('uid', uid);
+                router.push('/home');
+            } else {
+                router.push('/login')
+                alert("emailもしくはpasswordが間違っています。")
+            };
         };
     };
 
