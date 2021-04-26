26159022d88fd73ec396432b2d8ccb88ca4e9930
tanakafumiaki <fumiakishodai@gmail.com>
2021-04-26 20:47:00 +0900

    add_clearButton

--- src/components/atoms/AddButton/index.tsx
+++ src/components/atoms/AddButton/index.tsx
@@ -1,13 +1,15 @@
 import React from "react";
 import styles from "./styles.module.sass";
+import text from "node-html-parser/dist/nodes/text";
 
 interface Props {
     onClick: () => void;
+    text: string
 }
 
-const AddButton: React.VFC<Props> = ({ onClick }) => {
+const AddButton: React.VFC<Props> = ({ onClick, text }) => {
     return (
-        <button className={styles.addButton} onClick={onClick}>Add</button>
+        <button className={styles.addButton} onClick={onClick}>{text}</button>
     )
 }
 
