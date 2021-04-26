ef087eaaaedfabf9a4097aa91695e0ac7b9602ce
masataka moroe <masaozozoz91@gmail.com>
2021-04-26 16:06:20 +0900

    本番URL 変更 エラーメッセージ修正

--- src/components/atoms/ValidationForm/index.tsx
+++ src/components/atoms/ValidationForm/index.tsx
@@ -38,8 +38,8 @@ const ValidationForm = () => {
 
     const onClickLogin = async () => {
         // localで確認する場合は以下
-        const response = await fetch("http://localhost:3000/api/v1/auth/sign_in", {
-            // const response = await fetch("https://bullentin-board-api.herokuapp.com/api/v1/auth/sign_in", {
+        //const response = await fetch("http://localhost:3000/api/v1/auth/sign_in", {
+        const response = await fetch("https://bullentin-board-api.herokuapp.com/api/v1/auth/sign_in", {
             body: JSON.stringify({
                 email,
                 password
@@ -61,7 +61,7 @@ const ValidationForm = () => {
             router.push('/home');
         } else {
             router.push('/login')
-            alert("エラーが発生しました")
+            alert("ログインまたはパスワードが間違っています")
         };
     };
 
