26d7cf662d94e5f575936077142e559d45c342e9
masataka moroe <masaozozoz91@gmail.com>
2021-05-06 09:45:03 +0900

    sessionstrageのsetitem修正

--- src/components/organisms/SignUpValidationForm/index.tsx
+++ src/components/organisms/SignUpValidationForm/index.tsx
@@ -31,6 +31,12 @@ const SignUpValidationForm = () => {
         if (response.status === 200) {
             const { data } = await response.json();
             (data);
+            const accessToken: any = response.headers.get("access-token");
+            const client: any = response.headers.get("client");
+            const uid: any = response.headers.get("uid");
+            sessionStorage.setItem('access-token', accessToken);
+            sessionStorage.setItem('client', client);
+            sessionStorage.setItem('uid', uid);
             router.push('/home');
         } else {
             alert("メールアドレスまたはパスワードが間違っています")
